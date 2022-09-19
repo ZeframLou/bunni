bd38ec7f5a1a547d5d5750ace2f9ac1903d842ae
ZeframLou <zeframlou@gmail.com>
2022-09-19 08:54:03 -0700

    build: deploy to arbitrum, optimism, polygon

--- deploy/common.sh
+++ deploy/common.sh
@@ -2,7 +2,7 @@ ADDRESSES_FILE=${ADDRESSES_FILE:-./deployments/output.json}
 RPC_URL=${RPC_URL:-http://localhost:8545}
 
 deploy() {
-	RAW_RETURN_DATA=$(forge script script/Deploy.s.sol:Deploy --rpc-url $RPC_URL -vvvv --json --silent --broadcast --verify)
+	RAW_RETURN_DATA=$(forge script script/Deploy.s.sol:Deploy --rpc-url $RPC_URL -vvvv --json --silent --broadcast --verify --skip-simulation)
 	RETURN_DATA=$(echo $RAW_RETURN_DATA | jq -r '.returns' 2> /dev/null)
 
 	hub=$(echo $RETURN_DATA | jq -r '.hub.value')
